b1a8ac801658a3676fb6d778e9fdb78069e38e98
DESKTOP-I8S375Q\kunhee <tha920@naver.com>
2021-03-26 22:39:04 +0900

    gnb에 contact 메뉴 아이템 추가,
    커스텀 404 에러 페이지 추가

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Icon } from "semantic-ui-react";
+
+const errorPage = () => {
+  return (
+    <div style={{ padding: "200px 0", textAlign: "center", fontSize: 30 }}>
+      <Icon name="warning circle" color="red"></Icon>
+      404 : 페이지를 찾을 수 없습니다.
+    </div>
+  );
+};
+
+export default errorPage;
