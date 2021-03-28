f3b820fde60e22041a9aa1ffa0a9749d8084d135
DESKTOP-I8S375Q\kunhee <tha920@naver.com>
2021-03-28 21:29:18 +0900

    index 페이지를 정적 페이지로 변경 완료

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,29 @@
-import axios from "axios";
+import Axios from "axios";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import { Divider, Header, Loader } from "semantic-ui-react";
 import ItemList from "../src/component/ItemList";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+export default function Home({ list }) {
+  // const [list, setList] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-  // const API_URL =
-  //   "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+  // const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  // // const API_URL =
+  // //   "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
-  function getData() {
-    axios.get(API_URL).then((res) => {
-      console.log(res);
-      setList(res.data);
-      setIsLoading(false);
-    });
-  }
+  // function getData() {
+  //   Axios.get(API_URL).then((res) => {
+  //     console.log(res);
+  //     setList(res.data);
+  //     setIsLoading(false);
+  //   });
+  // }
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
   return (
     <div>
@@ -34,14 +34,14 @@ export default function Home() {
           content="LKHcoding 홈입니다. - Next.js tutorial"
         ></meta>
       </Head>
-      {isLoading && (
+      {/* {isLoading && (
         <div style={{ padding: "300px 0" }}>
           <Loader inline="centered" active>
             Loading
           </Loader>
         </div>
-      )}
-      {!isLoading && (
+      )} */}
+      {/* {!isLoading && (
         <>
           <Header as="h3" style={{ paddingTop: 40 }}>
             베스트 상품
@@ -54,7 +54,32 @@ export default function Home() {
           <Divider />
           <ItemList list={list.slice(9)} />
         </>
-      )}
+      )} */}
+      <>
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          베스트 상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(0, 9)} />
+        <Header as="h3" style={{ paddingTop: 40 }}>
+          신상품
+        </Header>
+        <Divider />
+        <ItemList list={list.slice(9)} />
+      </>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const apiUrl = process.env.apiUrl;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
 }
